b2f5f79dc6604c634a3939542b7b47400b863db3
Austin Davis <austindanieldavis@gmail.com>
2018-11-29 02:24:12 -0600

    just a few last minute changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
-import rick from './images/rick.jpg'
+import rick from './images/rick.jpg';
+import batmanWithText from './images/batmanWithText.jpg';
 
 // let App = (props) => {
 //     if (props.pizza) {
@@ -14,11 +15,12 @@ class App extends Component {
             text: " I like to use the word 'stateful' in casual conversation.",
             placeholder: "Type something here!",
             loadBtnText: "Load",
+            loadBtnStyle: { margin: '1em', borderRadius: '0.25em', fontSize: '1em', backgroundColor: '#343a40', borderColor: '#868e96', color: '#f8f9fa' },
             hasLoaded: false,
             loadingStyle: { display: 'none' },
             loadingText: "Loading",
-            rickStyle: { display: 'none' }
-
+            imgContent: rick,
+            imgStyle: { display: 'none' }
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleLoadBtnClick = this.handleLoadBtnClick.bind(this);
@@ -30,6 +32,7 @@ class App extends Component {
 
     handleLoadBtnClick() {
 
+
         if (this.state.hasLoaded === false) {
 
             this.setState({ loadingStyle: { display: 'block', fontSize: '2em', fontFamily: 'Orbitron, sans-serif' } });
@@ -40,16 +43,40 @@ class App extends Component {
                     setTimeout(() => {
                         this.setState({ loadingText: "Loading . . ." });
                         setTimeout(() => {
-                            this.setState({ hasLoaded: true, loadingStyle: { display: 'none' }, loadBtnText: "Make it go away!", rickStyle: { display: 'block', margin: 'auto', width: '80%' } });
+                            this.setState({
+                                hasLoaded: true,
+                                loadingStyle: { display: 'none' },
+                                loadBtnText: "Make it go away!",
+                                loadBtnStyle: { margin: '1em', borderRadius: '0.25em', fontSize: '1em', backgroundColor: '#ffc107', borderColor: '#495057', color: '#212529' },
+                                imgContent: rick,
+                                imgStyle: { display: 'block', margin: 'auto', width: '80vh' }
+                            });
                         }, 1000);
                     }, 1000);
                 }, 1000);
             }, 1000);
 
         } else if (this.state.hasLoaded === true) {
-            this.setState({ hasLoaded: false, loadBtnText: "Load", rickStyle: { display: 'none' } });
+
+            this.setState({
+                imgStyle: { display: 'none' },
+                imgStyle: { margin: 'auto', height: '50vh', display: 'block' },
+                imgContent: batmanWithText
+            });
+
+            setTimeout(() => {
+                this.setState({
+                    imgStyle: { display: 'none' },
+                    imgContent: rick,
+                    loadBtnText: "Load",
+                    loadBtnStyle: { margin: '1em', borderRadius: '0.25em', fontSize: '1em', backgroundColor: '#343a40', borderColor: '#868e96', color: '#f8f9fa' },
+                    hasLoaded: false,
+                });
+            }, 1800);
         }
     }
+
+
 
     render() {
         if (this.props.pizza) {
@@ -66,11 +93,11 @@ class App extends Component {
                         />
                     </div>
                     <button
-                        style={{ margin: '1em', borderRadius: '0.25em', fontSize: '1em', backgroundColor: '#343a40', borderColor: '#868e96', color: '#f8f9fa' }}
+                        style={this.state.loadBtnStyle}
                         onClick={(event) => { this.handleLoadBtnClick(event.target) }}
                     >{this.state.loadBtnText}</button>
                     <div style={this.state.loadingStyle}>{this.state.loadingText}</div>
-                    <img src={rick} alt={'Rick'} style={this.state.rickStyle} />
+                    <img src={this.state.imgContent} alt={'Rick'} style={this.state.imgStyle} />
                     <p style={{ fontFamily: 'Righteous, cursive', fontSize: '3em' }}>{this.state.inputVal}</p>
                 </div>
             )
